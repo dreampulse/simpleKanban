ef4ed2edcf5dc119d157e1b7f69431ebf03c79b5
Jonathan Häberle <jonathan.haeberle@gmail.com>
2014-09-19 22:42:53 +0200

    Strict Model added

--- app/index.js
+++ app/index.js
@@ -2,38 +2,91 @@
 angular.module('app', ['ui.sortable', 'ngStorage']).run(function () {
 });
 
+var Item = (function () {
+    function Item(item) {
+        if (item) {
+            this.content = item.content;
+        }
+    }
+    return Item;
+})();
+
+var Column = (function () {
+    function Column(column) {
+        if (column) {
+            this.name = column.name;
+            this.items = column.items.map(function (item) {
+                return new Item(item);
+            });
+        }
+    }
+    Column.prototype.add = function () {
+        this.items.push({ content: '' });
+    };
+
+    Column.prototype.remove = function (idx) {
+        this.items.splice(idx, 1);
+    };
+
+    Column.prototype.assureOneEmptyItem = function () {
+        var last = this.items[this.items.length - 1];
+        if (last.content === undefined || last.content !== "") {
+            this.add();
+        }
+    };
+    return Column;
+})();
+
+var Model = (function () {
+    function Model(model) {
+        if (model) {
+            this.columns = model.columns.map(function (column) {
+                return new Column(column);
+            });
+        }
+    }
+    Model.prototype.assureOneEmptyItem = function () {
+        this.columns.forEach(function (colum) {
+            return colum.assureOneEmptyItem();
+        });
+    };
+    return Model;
+})();
+
 var AppCtrl = (function () {
     function AppCtrl($scope, $localStorage) {
-        var init = [
-            {
-                name: 'open',
-                items: [
-                    { content: 'drink beer' },
-                    { content: 'make good presentation' }
-                ] },
-            {
-                name: 'progress',
-                items: [
-                    { content: 'learn CSS' }
-                ] },
-            {
-                name: 'done',
-                items: [
-                    { content: 'eat pizza' }
-                ] }
-        ];
+        var _this = this;
+        var init = { columns: [
+                {
+                    name: 'open',
+                    items: [
+                        { content: 'drink beer' },
+                        { content: 'make good presentation' }
+                    ] },
+                {
+                    name: 'progress',
+                    items: [
+                        { content: 'learn CSS' }
+                    ] },
+                {
+                    name: 'done',
+                    items: [
+                        { content: 'eat pizza' }
+                    ] }
+            ] };
 
-        this.columns = $localStorage.$default({
-            store: init
-        }).store;
+        var storage = $localStorage.$default({ model: init });
+
+        this.vm = new Model(storage.model);
+
+        $scope.$watch(function () {
+            return _this.vm;
+        }, function (vm) {
+            vm.assureOneEmptyItem();
+
+            storage.model = vm; // store change in database
+        }, true);
     }
-    AppCtrl.prototype.add = function (idx) {
-        this.columns[idx].items.push({ content: '' });
-    };
-
-    AppCtrl.prototype.remove = function (idxColumn, idxItem) {
-        this.columns[idxColumn].items.splice(idxItem, 1);
-    };
     return AppCtrl;
 })();
 angular.module('app').controller('AppCtrl', AppCtrl);
--- app/index.ts
+++ app/index.ts
@@ -3,43 +3,95 @@
 angular.module('app', ['ui.sortable', 'ngStorage']).run(function () {
 });
 
+class Item {
+    content:string;
+
+    constructor(item?:Item) {
+        if (item) {
+            this.content = item.content;
+        }
+    }
+}
+
+class Column {
+    name:string;
+    items:Item[];
+
+    constructor(column?:Column) {
+        if (column) {
+            this.name = column.name;
+            this.items = column.items.map(item => {
+                return new Item(item);
+            })
+        }
+    }
+
+    public add() {
+        this.items.push({content: ''});
+    }
+
+    public remove(idx:number) {
+        this.items.splice(idx, 1);
+    }
+
+    public assureOneEmptyItem() {
+        var last = this.items[this.items.length-1];
+        if (last.content === undefined || last.content !== "") {
+            this.add();
+        }
+    }
+}
+
+class Model {
+    columns:Column[];
+
+    constructor(model?:Model) {
+        if (model) {
+            this.columns = model.columns.map(column => {
+                return new Column(column);
+            })
+        }
+    }
+
+    public assureOneEmptyItem() {
+        this.columns.forEach(colum => colum.assureOneEmptyItem());
+    }
+
+}
+
 class AppCtrl {
-    public columns:{
-        name : string;
-        items : {
-            content : string
-        }[]
-    }[];
+    public vm:Model;
 
     constructor($scope:ng.IScope, $localStorage:any) {
 
-        var init = [
-            { name: 'open',
-                items: [
-                    {content: 'drink beer'},
-                    {content: 'make good presentation'}
-                ]},
-            { name: 'progress',
-                items: [
-                    {content: 'learn CSS'}
-                ]},
-            { name: 'done',
-                items: [
-                    {content: 'eat pizza'}
-                ]}
-        ];
+        var init = {
+            columns: [
+                { name: 'open',
+                    items: [
+                        {content: 'drink beer'},
+                        {content: 'make good presentation'}
+                    ]},
+                { name: 'progress',
+                    items: [
+                        {content: 'learn CSS'}
+                    ]},
+                { name: 'done',
+                    items: [
+                        {content: 'eat pizza'}
+                    ]}
+            ]};
 
-        this.columns = $localStorage.$default({
-            store: init
-        }).store;
+        var storage = $localStorage.$default({ model: init });
+
+        this.vm = new Model(storage.model);
+
+        $scope.$watch(() => {return this.vm}, (vm) => {
+
+            vm.assureOneEmptyItem();
+
+            storage.model = vm;  // store change in database
+        }, true);
     }
 
-    public add(idx) {
-        this.columns[idx].items.push({content : ''});
-    }
-
-    public remove(idxColumn, idxItem) {
-        this.columns[idxColumn].items.splice(idxItem, 1);
-    }
 }
 angular.module('app').controller('AppCtrl', AppCtrl);
