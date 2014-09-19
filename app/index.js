3b0bc1729c1cb25fd6572898ef110b023e9a72f9
Jonathan Häberle <jonathan.haeberle@gmail.com>
2014-09-19 13:38:10 +0200

    ngStorage added

--- app/index.js
+++ app/index.js
@@ -1,37 +1,35 @@
 /// <reference path='../typings/tsd.d.ts'/>
-angular.module('app', ['ui.sortable']).run(function () {
+angular.module('app', ['ui.sortable', 'ngStorage']).run(function () {
 });
 
-var List = (function () {
-    function List(name, items) {
-        this.name = name;
-        this.items = items.map(function (e) {
-            return { content: e };
-        });
-    }
-    List.prototype.add = function () {
-        this.items.push({
-            content: ''
-        });
-    };
-    return List;
-})();
-
 var AppCtrl = (function () {
-    function AppCtrl($scope) {
-        this.lists = [
-            new List('open', [
-                'drink beer',
-                'make good presentation'
-            ]),
-            new List('progress', [
-                'learn CSS'
-            ]),
-            new List('done', [
-                'eat pizza'
-            ])
+    function AppCtrl($scope, $localStorage) {
+        var init = [
+            {
+                name: 'open',
+                items: [
+                    { content: 'drink beer' },
+                    { content: 'make good presentation' }
+                ] },
+            {
+                name: 'progress',
+                items: [
+                    { content: 'learn CSS' }
+                ] },
+            {
+                name: 'done',
+                items: [
+                    { content: 'eat pizza' }
+                ] }
         ];
+
+        this.lists = $localStorage.$default({
+            store: init
+        }).store;
     }
+    AppCtrl.prototype.add = function (idx) {
+        this.lists[idx].items.push({ content: '' });
+    };
     return AppCtrl;
 })();
 angular.module('app').controller('AppCtrl', AppCtrl);
--- app/index.ts
+++ app/index.ts
@@ -1,42 +1,41 @@
 /// <reference path='../typings/tsd.d.ts'/>
 
-angular.module('app', ['ui.sortable']).run(function() { });
-
-class List {
-    public name : string;
-    public items : {
-        content : string;
-    }[];
-
-    constructor(name : string, items : string[]) {
-        this.name = name;
-        this.items = items.map(e => { return {content:e} } );
-    }
-
-    public add() {
-        this.items.push({
-            content : ''
-        })
-    }
-}
+angular.module('app', ['ui.sortable', 'ngStorage']).run(function () {
+});
 
 class AppCtrl {
-    public lists : List[];
+    public lists:{
+        name : string;
+        items : {
+            content : string
+        }[]
+    }[];
 
-    constructor($scope : ng.IScope) {
-        this.lists = [
-            new List('open', [
-                'drink beer',
-                'make good presentation'
-            ]),
-            new List('progress', [
-                'learn CSS'
-            ]),
-            new List('done', [
-                'eat pizza'
-            ])
+    constructor($scope:ng.IScope, $localStorage:any) {
+
+        var init = [
+            { name: 'open',
+                items: [
+                    {content: 'drink beer'},
+                    {content: 'make good presentation'}
+                ]},
+            { name: 'progress',
+                items: [
+                    {content: 'learn CSS'}
+                ]},
+            { name: 'done',
+                items: [
+                    {content: 'eat pizza'}
+                ]}
         ];
-        
+
+        this.lists = $localStorage.$default({
+            store: init
+        }).store;
+    }
+
+    public add(idx) {
+        this.lists[idx].items.push({content : ''});
     }
 }
 angular.module('app').controller('AppCtrl', AppCtrl);
